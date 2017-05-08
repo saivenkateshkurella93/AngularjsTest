7d86cdf54b82c358c10ae50daabe96c7081608cf
saivenkateshkurella <saivenkatesh.kurella@hitachiconsulting.com>
2017-05-08 20:27:32 +0530

    Add project files.

---
+++ AngularjsTest/Controller/AngularjsTest.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="D:\AngularjsTest\AngularjsTest\angular.min.js" />
+//angular.module("myapp", [])
+
+
+
+    
+//        .controller("HelloController", function ($scope) {
+//            $scope.helloTo = {};
+//            $scope.helloTo.title = "AngularJS";
+//        });
+
+
+ 
+
+var myApp = angular.module("myapp", []); //module and its dependencies
+
+var control = function ($scope) {
+    $scope.helloTo = {};
+          $scope.helloTo.title = "AngularJS";
+};
+
+myApp.controller("HelloController", control); //myApp is module and control is controller function  
