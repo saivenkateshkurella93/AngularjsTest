1c236f9897e284b50ba63fabb50d66a05ec4d166
saivenkateshkurella <saivenkatesh.kurella@hitachiconsulting.com>
2017-05-08 20:44:22 +0530

    test

--- AngularjsTest/Controller/AngularjsTest.js
+++ AngularjsTest/Controller/AngularjsTest.js
@@ -1,22 +1,22 @@
-﻿/// <reference path="D:\AngularjsTest\AngularjsTest\angular.min.js" />
-//angular.module("myapp", [])
+﻿// <reference path="D:\AngularjsTest\AngularjsTest\angular.min.js" />
+angular.module("myapp", [])
 
 
 
     
-//        .controller("HelloController", function ($scope) {
-//            $scope.helloTo = {};
-//            $scope.helloTo.title = "AngularJS";
-//        });
+        .controller("HelloController", function ($scope) {
+            $scope.helloTo = {};
+            $scope.helloTo.title = "AngularJS";
+        });
 
 
  
 
-var myApp = angular.module("myapp", []); //module and its dependencies
+//var myApp = angular.module("myapp", []); //module and its dependencies
 
-var control = function ($scope) {
-    $scope.helloTo = {};
-          $scope.helloTo.title = "AngularJS";
-};
+//var control = function ($scope) {
+//    $scope.helloTo = {};
+//           $scope.helloTo.title = "AngularJS";
+//};
 
-myApp.controller("HelloController", control); //myApp is module and control is controller function  
+//myApp.controller("HelloController", control); //myApp is module and control is controller function  
