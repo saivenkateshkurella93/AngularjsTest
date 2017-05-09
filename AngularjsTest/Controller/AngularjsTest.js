fad4285c981b156c4fe5f5af2021880808b293d4
saivenkateshkurella <saivenkatesh.kurella@hitachiconsulting.com>
2017-05-09 19:09:26 +0530

    Filter, loop implemented.

--- AngularjsTest/Controller/AngularjsTest.js
+++ AngularjsTest/Controller/AngularjsTest.js
@@ -7,8 +7,32 @@ angular.module("myapp", [])
         .controller("HelloController", function ($scope) {
             $scope.helloTo = {};
             $scope.helloTo.title = "AngularJS";
-$scope.showMessage = function() {
-   console.log("Hello!!!!!!!!");
+            $scope.showDate = function () {
+                console.log("Hello!!!!!!!!");
+                var d = new Date();
+                console.log(d.getDate());
+                $scope.helloTo.showDate = d.getDate()+ '/' + (d.getMonth()+1) + '/' +d.getFullYear();
+            };
+            $scope.loopObjects = function () {
+
+                $scope.names = [
+        { name: 'Venky', country: 'India' },
+        { name: 'Sai', country: 'USA' },
+        { name: 'Gattanna', country: 'England' },
+        { name: 'Yesh', country: 'Norway' },
+        { name: 'Fareed', country: 'Afghanistan' },
+        { name: 'Avinash', country: 'Sweden' },
+        { name: 'Madhu', country: 'France' },
+        { name: 'Maheshs', country: 'Finland' },
+        { name: 'Swaroop', country: 'Chile' },
+        { name: 'Kanthi', country: 'Peru' },
+        { name: 'Pakki', country: 'Japan' },
+         { name: 'Bro', country: 'China' },
+
+
+
+
+                ];
     };
         });
 
